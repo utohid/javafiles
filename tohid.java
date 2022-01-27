f2f1255b7d471f4fa2e88f1892d17e78a86b2c38
utohid <noortohid78@gmail.com>
2022-01-27 13:30:53 +0530

    commit in java files main branch

---
+++ tohid.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class tohid
+{
+    public static void maim {
+     int a=5;
+     int b=12;
+     int c=a+b;
+     System.out.println("Addition of the number is "+c);
+   }
+
+}
